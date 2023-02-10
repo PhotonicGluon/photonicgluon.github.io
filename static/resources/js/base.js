b04d420b910c182a5562684f689b87c255f90dd4
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2023-02-10 14:05:43 +0800

    Add rudimentary projects page

---
+++ static/resources/js/base.js
@@ -0,0 +1,17 @@
+// Configure "back to top" button
+let backToTopButton = $("#back-to-top");
+
+$(window).scroll(() => {
+    let scrollTop = $(window).scrollTop();
+    if (scrollTop > $("#hero-box").height()) {
+        // Show the button
+        backToTopButton.css("visibility", "visible");
+        backToTopButton.css("opacity", 1);
+        backToTopButton.css("transform", "scale(1)");
+    } else {
+        // Hide the button
+        backToTopButton.css("visibility", "hidden");
+        backToTopButton.css("opacity", 0);
+        backToTopButton.css("transform", "scale(0)");
+    }
+});
--- static/resources/js/index.js
+++ static/resources/js/index.js
@@ -1,21 +1,3 @@
-// Configure "back to top" button
-let backToTopButton = $("#back-to-top");
-
-$(window).scroll(() => {
-    let scrollTop = $(window).scrollTop();
-    if (scrollTop > $("#featured-projects").position().top) {
-        // Show the button
-        backToTopButton.css("visibility", "visible");
-        backToTopButton.css("opacity", 1);
-        backToTopButton.css("transform", "scale(1)");
-    } else {
-        // Hide the button
-        backToTopButton.css("visibility", "hidden");
-        backToTopButton.css("opacity", 0);
-        backToTopButton.css("transform", "scale(0)");
-    }
-});
-
 // Configure colours for labels
 $("#featured-projects-container").find("span").each((i, obj) => {
     $(obj).addClass(`featured-project-tag-${obj.innerText.toLowerCase()}`);
