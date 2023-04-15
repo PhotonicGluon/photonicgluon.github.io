313758ee70c8b42f4514614179e225a853ce7f28
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2023-04-15 19:22:09 +0800

    Some work on blog content

---
+++ assets/resources/js/post.js
@@ -0,0 +1,5 @@
+// Update hero box tags
+$("#hero-box-content").html(`
+    <h1 id="hero-box-post-name">${$("#post-name").html()}</h1>
+    <h2 id="hero-box-post-date">${$("#post-date").html()}</h2>
+`);
