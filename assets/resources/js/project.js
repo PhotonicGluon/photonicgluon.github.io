c82d2e5023a50ae9aaf72154b6b4a0563f0f9d49
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2023-08-17 14:47:18 +0800

    Update project showing pages

--- assets/resources/js/project.js
+++ assets/resources/js/project.js
@@ -1,45 +1,13 @@
-// Define showdown converter and image wrapping extension
-// Todo: deprecate Showdown
-let imageWrapperExt = {
-    type: "output",
-    filter: (src) => {
-        return src.replace(
-            /<p><img .+?><\/p>/g,
-            (x) => `<div class="image-wrapper">${x.substring(3, x.length - 4)}</div>`
-        );
-    }
-};
-showdown.extension("image_wrapper", imageWrapperExt);
-let converter = new showdown.Converter({extensions: ["image_wrapper"]});
-
 // Retrieve project data
 let pageURL = window.location.href;
-pageURL = pageURL.substring(0, pageURL.length-1);  // Remove trailing slash
-
 let projectID = pageURL.substring(pageURL.lastIndexOf("/") + 1);
-let baseURL = PROJECTS_FOLDER + projectID + "/";
 
 $.ajax(PROJECTS_FILE, {
     success: (data) => {
         let project = data[projectID];
 
-        let title = $("title");
-        let heroBoxProjectName = $("#hero-box-project-name");
-        if (project == null) {
-            title.text("Project Not Found | Overwrite");
-            heroBoxProjectName.text("Project Not Found");
-            $("#hero-box-project-duration").remove();
-
-            let projectSection = $("#project");
-            projectSection.children().remove();
-            projectSection.css("padding-bottom", "30px");
-            projectSection.append(`<span id="project-not-found">The requested project could not be found.</span>`);
-            return;
-        }
-
         // Update main details
-        title.text(project["name"] + " | Overwrite");
-        heroBoxProjectName.text(project["name"]);
+        $("#hero-box-project-name").text(project["name"]);
         $("#hero-box-project-start-date").text(project["start_date"]);
         $("#hero-box-project-end-date").text(project["end_date"]);
 
@@ -74,19 +42,5 @@ $.ajax(PROJECTS_FILE, {
                 <img src="/assets/vendors/img/internet.svg" alt="Website Icon">
             </a>
         </li>`);
-
-        // Retrieve the actual data to show
-        if (project["page_type"] === "markdown") {
-            $.ajax(baseURL + projectID + ".md", {
-                success: (code) => {
-                    let outputHTML = converter.makeHtml(code);
-                    $("#project-desc").html(outputHTML);
-                }
-            });
-        } else {
-            $.ajax(baseURL + projectID + ".html", {
-                success: (code) => $("#project-desc").html(code)
-            });
-        }
     }
 });
